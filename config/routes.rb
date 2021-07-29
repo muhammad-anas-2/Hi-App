a4410c6ac4dc26826cf865ab1b541314bbcdc04a
Al-hafiz Academy App <itsranas517@gmail.com>
2021-07-29 18:22:35 +0500

    Add message from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 
 end
